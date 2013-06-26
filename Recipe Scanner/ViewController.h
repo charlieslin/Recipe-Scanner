430d3576cb40711e7a6caa92660274fa4430dd87
Charlie Lin <charlie@tribaltech.com>
2013-06-25 21:32:23 -0700

    Initial Commit

---
+++ Recipe Scanner/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Recipe Scanner
+//
+//  Created by Charlie Lin on 6/25/13.
+//  Copyright (c) 2013 Charlie Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Recipe ScannerTests/Recipe_ScannerTests.m
@@ -0,0 +1,36 @@
+//
+//  Recipe_ScannerTests.m
+//  Recipe ScannerTests
+//
+//  Created by Charlie Lin on 6/25/13.
+//  Copyright (c) 2013 Charlie Lin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Recipe_ScannerTests : XCTestCase
+
+@end
+
+@implementation Recipe_ScannerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
